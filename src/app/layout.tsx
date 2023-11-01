03198ae5c07c78e58790637f8fb96bb7d85e0ee3
scholak <scholaksebahattin@gmail.com>
2023-11-01 15:50:44 +0300

    fix: client providers added to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import RootProvider from '@/providers/RootProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+		<html lang='en'>
+			<body className={inter.className}>
+				<RootProvider>{children}</RootProvider>
+			</body>
+		</html>
+	)
 }
