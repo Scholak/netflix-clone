2b2e2834510eb56d14aa742213d326cb5eec90f5
scholak <scholaksebahattin@gmail.com>
2023-11-03 14:10:44 +0300

    feat: pay via phone number page completed

---
+++ src/app/signup/dcboption/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PayViaPhonePage = () => {
+  return (
+    <div className='my-6 text-center text-5xl font-bold'>Not implemented</div>
+  )
+}
+
+export default PayViaPhonePage
