6d02847fff771666f256828426a27dafa76dedd9
scholak <scholaksebahattin@gmail.com>
2023-11-03 15:19:02 +0300

    fix: credit card number lengt fixed

--- src/validations/payWithCardSchema.ts
+++ src/validations/payWithCardSchema.ts
@@ -2,7 +2,7 @@ import { IPayWithCard } from '@/types/forms/payWithCardType'
 import { ZodType, z } from 'zod'
 
 export const payWithCardSchema: ZodType<IPayWithCard> = z.object({
-	cardNumber: z.string().length(12, 'Lütfen geçerli bir kredi kartı numarası girin.'),
+	cardNumber: z.string().length(16, 'Lütfen geçerli bir kredi kartı numarası girin.'),
 	expiryDate: z.string().min(1, 'Lütfen Geçerli bir tarih girin.'),
 	cvv: z.string().length(3, 'Lütfen geçerli bir CVV girin.'),
 	cardHolder: z.string().min(1, 'Lütfen geçerli bir isim girin.'),
