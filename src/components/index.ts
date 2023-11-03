3fca3c3b609e33df13b87c2fa226f9e864976143
scholak <scholaksebahattin@gmail.com>
2023-11-03 15:09:49 +0300

    fix: HomeContent component import removed

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
-export { default as HomeContent } from './Contents/HomeContent'
 export { default as GetStartedForm } from './Forms/GetStartedForm'
 export { default as RegisterForm } from './Forms/RegisterForm'
 export { default as PlanForm } from './Forms/PlanForm'
