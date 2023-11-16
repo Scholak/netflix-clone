cb6c06e1014418950cd132b0a19a0bf5dd21f1a8
scholak <scholaksebahattin@gmail.com>
2023-11-16 22:30:32 +0300

    refactor: user home page vertical padding added to slider

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -9,10 +9,12 @@ const UserHomePage = async () => {
   return (
 		<div className='bg-neutral-900'>
 			<Banner mediaType='movie' />
-			<MovieSlider title='Gündemdekiler' link='/popular' items={popularMovieResponse.data.movies} />
-			{sliderMoviesResponse.data.sliders.map((slider: any) => (
-				<MovieSlider key={slider.id} title={slider.title} link={slider.link} items={slider.items} />
-			))}
+			<div className='py-4 md:py-6 lg:py-12'>
+				<MovieSlider title='Gündemdekiler' link='/popular' items={popularMovieResponse.data.movies} />
+				{sliderMoviesResponse.data.sliders.map((slider: any) => (
+					<MovieSlider key={slider.id} title={slider.title} link={slider.link} items={slider.items} />
+				))}
+			</div>
 		</div>
 	)
 }
