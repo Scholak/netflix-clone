40cc6fc106365c43d1996e9449defdbdc103fc9a
scholak <scholaksebahattin@gmail.com>
2023-11-19 16:09:47 +0300

    refactor: empty search result condition checked

--- src/app/user/search/page.tsx
+++ src/app/user/search/page.tsx
@@ -19,10 +19,19 @@ const SearchPage = async ({ searchParams }: ISearchParams) => {
   const serieResponse = await api.get(`/series/search?q=${searchParams.q}`)
 
   return (
-		<div className='p-4 py-4 md:py-6 lg:py-20 md:p-8 xl:px-16 bg-neutral-900'>
-			<h5 className='flex items-end gap-2 text-3xl text-white font-bold mb-4 md:mb-8'>
-				<span>{searchParams.q}</span>
-				<span className='text-neutral-300 text-lg font-medium'>için arama sonuçları</span>
+		<div className='p-4 py-4 py-20 md:p-8 xl:px-16 bg-neutral-900'>
+			<h5 className='flex items-end gap-2 text-3xl text-white font-bold mb-auto'>
+			{movieResponse.data.movies.length === 0 && serieResponse.data.series.length === 0 ? (
+				<>
+					<span>{searchParams.q}</span>
+					<span className='text-neutral-300 text-lg font-medium'>için sonuç bulunamadı</span>
+				</>
+			) : (
+				<>
+					<span>{searchParams.q}</span>
+					<span className='text-neutral-300 text-lg font-medium'>için arama sonuçları</span>
+				</>
+			)}
 			</h5>
 			<div className='grid gap-4 md:gap-y-16 lg:gap-y-32 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
 				{movieResponse.data.movies.map((movie: any) => (
