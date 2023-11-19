92757e3414bae32d9f86df832fab17922794e143
scholak <scholaksebahattin@gmail.com>
2023-11-19 22:54:19 +0300

    refactor: margin bottom added to list title

--- src/app/user/list/page.tsx
+++ src/app/user/list/page.tsx
@@ -22,7 +22,7 @@ const ListPage = async () => {
 
   return (
 		<div className='px-4 py-20 md:px-8 xl:px-16 bg-neutral-900'>
-			<h5 className='flex flex-wrap items-end gap-2 text-3xl text-white font-bold mb-auto'>Listem</h5>
+			<h5 className='flex flex-wrap items-end gap-2 text-3xl text-white font-bold mb-4 md:mb-8'>Listem</h5>
 			<div className='grid gap-4 md:gap-y-16 lg:gap-y-32 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
 				{response.data.lists.map((list: any) => (
 					<SearchCard media={list} type={list.type} key={list.id} />
