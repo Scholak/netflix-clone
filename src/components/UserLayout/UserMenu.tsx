acd7dc266c11396a156d5242efd718bd08f2e6c0
scholak <scholaksebahattin@gmail.com>
2023-11-19 23:05:33 +0300

    feat: switch profile functionality added

--- src/components/UserLayout/UserMenu.tsx
+++ src/components/UserLayout/UserMenu.tsx
@@ -1,13 +1,18 @@
 'use client'
 
+import { queryClient } from '@/lib/queryClient'
 import { getProfiles } from '@/services/profileService'
 import { IProfile } from '@/types/profileType'
 import { useQuery } from '@tanstack/react-query'
-import { signOut } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import { FaCaretDown, FaFileExport, FaPencilAlt, FaRegQuestionCircle, FaRegUser } from 'react-icons/fa'
 
 const UserMenu = () => {
+	const session = useSession()
+	const router = useRouter()
+	
   const {
 		data: profiles,
 		isLoading,
@@ -25,6 +30,11 @@ const UserMenu = () => {
 		// skeleton loader will be implemented later...
 	}
 
+	const handleSwitchProfile = async (profileId: number) => {
+		await session.update({ profileId })
+		router.refresh()
+	}
+
   return (
 		<div className='group relative flex items-center gap-2 text-sm cursor-pointer'>
 			<div className='w-6 h-6 bg-red rounded'></div>
@@ -33,7 +43,11 @@ const UserMenu = () => {
 				<div className='bg-neutral-800 border border-neutral-300'>
 					<div className='grid gap-2 mb-3 p-3'>
 						{profiles?.map((profile: IProfile) => (
-							<div key={profile.id} className='flex items-center gap-3 hover:underline'>
+							<div
+								key={profile.id}
+								onClick={() => handleSwitchProfile(profile.id)}
+								className='flex items-center gap-3 hover:underline'
+							>
 								<div className='w-6 h-6 rounded bg-red'></div>
 								<p className='text-white'>{profile.name}</p>
 							</div>
