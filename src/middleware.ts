4e5bf25171e2655ee28e6013b33333d3277a370b
scholak <scholaksebahattin@gmail.com>
2023-11-21 17:04:43 +0300

    feat: auth middleware applied to api routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,14 @@
 export { default } from 'next-auth/middleware'
 
-export const config = { matcher: ['/browse', '/create-profile', '/manageProfiles/:path*', '/user/:path*'] }
+export const config = {
+	matcher: [
+		'/browse',
+		'/create-profile',
+		'/manageProfiles/:path*',
+		'/user/:path*',
+		'/api/profile/:path*',
+		'/api/movies/:path*',
+		'/api/series/:path*',
+		'/api/list/:path*',
+	],
+}
